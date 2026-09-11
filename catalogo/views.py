2b105f2c7d894006acc76194592bb2ef2a177c95
pallmall07 <javier.mamani08@inacapmail.cl>
2026-09-11 17:43:00 -0300

    creacion de la funcion en views.py incluyendo la importacion de de datos

--- catalogo/views.py
+++ catalogo/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 def mostrar_productos(request):
 
     productos = [
+        
+        {"nombre": "Fertilizante NPK 15-15-15", "categoria": "Fertilizantes", "precio": 18990, "stock": 120},
+        {"nombre": "Semilla de Maíz Híbrido", "categoria": "Semillas", "precio": 5990, "stock": 340},
+        {"nombre": "Herbicida Glifosato 20L", "categoria": "Agroquímicos", "precio": 45990, "stock": 25},
+
 
         
     ]
